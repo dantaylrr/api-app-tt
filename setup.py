2226f79ab729f4d81fedc08ff00ad29457561f03
dantaylrr <dannyyyyt@gmail.com>
2023-06-18 16:14:45 +0100

    Adding setup.py.

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name='kraken-technical-test',
+    version=0.1,
+    description="Workspace for Kraken technical test - sets us up for use of relative imports.",
+    packages=find_namespace_packages()
+)
